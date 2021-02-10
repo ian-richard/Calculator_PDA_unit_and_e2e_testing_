6465cd26294894058f3bf891943815d44c1ad8fc
Ian F <ian.fiddeman@gmail.com>
2021-02-10 13:36:53 +0000

    initial commit

---
+++ tests/unit/app.spec.js
@@ -0,0 +1,13 @@
+import { expect } from 'chai'
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+describe('App.vue', () => {
+  it('Num changes running total', () => {
+    const wrapper = shallowMount(App)
+    wrapper.vm.previousTotal = 4
+    wrapper.vm.add('1');
+    expect(wrapper.vm.runningTotal).to.equal(5);
+  })
+})
+
